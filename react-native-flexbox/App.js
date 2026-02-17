7af057b213178d2e9bfe2d11f3984224628ea436
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-02-18 06:37:42 +0800

    Done with flex section

---
+++ react-native-flexbox/App.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import Flex from './components/Flex';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Flex />
+    </View>
+  );
+}
