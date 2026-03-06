63d31b901c74739526a4e7d1ef5364abb9860403
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-07 06:57:08 +0800

    Implemented Todo screen with add, toggle, delete, and search functionality

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,13 +1,16 @@
 import { View } from 'react-native';
 // import Login from './screens/Login';
-import Signup from './screens/Signup';
+// import Signup from './screens/Signup';
+import Todo from './screens/Todo';
+
 
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       {/* <Login /> */}
-      <Signup />
+      {/* <Signup /> */}
+      <Todo />
     </View>
   );
 }
