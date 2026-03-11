b9996d58fb2359bf6edd701d9df7a9b84c137e71
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-11 17:31:47 +0800

    Done with flex direction section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,10 +1,12 @@
 import { View } from 'react-native';
-import Flex from './components/Flex.js';
+// import Flex from './components/Flex.js';
+import FlexDirection from './components/FlexDirection.js';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Flex />
+      {/* <Flex/> */}
+      <FlexDirection />
     </View>
   );
 }
