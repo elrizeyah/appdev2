a2aed085ef710af3e6bb19895f87dcc94c40823b
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-11 17:06:22 +0800

    Done with style section

---
+++ react-native-ui-n-interaction/App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Style from './components/Style.js';
+
+
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Style />
+    </View>
+  );
+}
